ba3c87ba14891cc7066ef15bb7575237285d74a1
Taras Mykhailovych <tarquas@gmail.com>
2021-03-02 22:57:15 +0200

    add wait.h support for windows

--- code/server/sv_ccmds.c
+++ code/server/sv_ccmds.c
@@ -24,7 +24,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/wait.h>
+#include "../wait.h"
 #include <spawn.h>
 
 #include "server.h"
